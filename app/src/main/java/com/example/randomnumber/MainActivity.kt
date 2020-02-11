d0f59ef972031408a0f7b13ad9eceb203e22cf6a
rey <rey.echeverria7@gmail.com>
2020-02-11 12:49:12 -0800

    App "funcional" sin personalizacion

--- app/src/main/java/com/example/randomnumber/MainActivity.kt
+++ app/src/main/java/com/example/randomnumber/MainActivity.kt
@@ -10,19 +10,57 @@ class MainActivity : AppCompatActivity() {
 
     var counter: Int = 0
     var num:Int = 0
+    var auxa: Int = 100
+    var auxb: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val etiqueta: TextView = findViewById(R.id.etiqueta) as TextView
         val down: Button = findViewById(R.id.down) as Button
+        down.isEnabled = false
         val up: Button = findViewById(R.id.up) as Button
+        up.isEnabled = false
         val generate: Button = findViewById(R.id.generate) as Button
 
+        val correcto:Button = findViewById(R.id.correcto)as Button
+
         generate.setOnClickListener{
-            num = Random.nextInt(0,100)
+
+                num = Random.nextInt(0,100)
+                etiqueta.setText(""+num+"")
+
+                up.isEnabled = true
+                down.isEnabled = true
+
+        }
+
+        up.setOnClickListener{
+
+            auxb = num
+
+            num = Random.nextInt(auxb,auxa)
+            etiqueta.setText(""+num+"")
+
+
+        }
+
+        down.setOnClickListener{
+            auxa = num
+
+            num = Random.nextInt(auxb,auxa)
              etiqueta.setText(""+num+"")
         }
+
+        correcto.setOnClickListener{
+            up.isEnabled=false
+            down.isEnabled = false
+            num = 0
+            etiqueta.setText("$num")
+            auxa = 100
+            auxb=0
+        }
+
 
     }
 }
